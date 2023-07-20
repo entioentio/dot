878e71a880485db2464fc3d56effc296c91c9f39
przemek <przemek@amber.team>
2023-07-20 09:06:30 +0200

    Update

--- scripts/check-battery.sh
+++ scripts/check-battery.sh
@@ -8,11 +8,11 @@ if echo "$battery_status" | grep -qi "discharging"; then
     is_charging=false
 else
     is_charging=true
-	exit 0
+    exit 0
 fi
 
-battery_level=$(acpi -b | awk 'NR==1{print $4}' | sed 's/%,//' | sed 's/%//')
-if [[ $battery_level -le $showAtPercent ]]; then
+battery_level=$(acpi -b | awk -F ', ' '/Battery 0/ {print $2}' | tr -d '%')
+if [ -n "$battery_level" ] && [ "$battery_level" -le "$showAtPercent" ]; then
     killall i3-nagbar >/dev/null 2>&1
     i3-nagbar -m "Battery is low! ($battery_level%) Please plug in your charger." -t warning >/dev/null 2>&1 &
 fi
